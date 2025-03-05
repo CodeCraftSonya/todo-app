76f51a1aa191e6c0f96a68082c7c83055c70620b
DESKTOP-L88LUBN\София <sofiya.fish02@mail.ru>
2025-03-05 16:17:50 +0300

    feat: add presenter

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,20 @@
+import { Form } from "./components/Form";
+import { Item } from "./components/item";
+import { Page } from "./components/Page";
+import { ToDoModel } from "./components/ToDoModel";
+import { ItemPresenter } from "./components/ToDoPresenter";
 import "./styles/styles.css";
 import { todos } from "./utils/constants";
-import { Item } from "./components/item";
-import { Form } from "./components/Form";
-import { ToDoModel } from "./components/ToDoModel";
 
-const contentElement = document.querySelector(".todos__list");
+const contentElement = document.querySelector(".content") as HTMLElement;
 
-const template = document.querySelector(
-  "#todo-item-template"
-) as HTMLTemplateElement;
-const formElement = document.querySelector(".todos__form") as HTMLFormElement;
-const todoForm = new Form(formElement, handleSubmitForm);
+const popupElement = document.querySelector(".popup") as HTMLElement;
 
-function handleSubmitForm(data: string) {
-  const todoItem = new Item(template);
-  const itemElement = todoItem.render({ id: "8", name: data });
-  contentElement.prepend(itemElement);
-  todoForm.clearValue();
-}
-
-todos.forEach((item) => {
-  const todoItem = new Item(template);
-  const itemElement = todoItem.render(item);
-  contentElement.prepend(itemElement);
-});
+const itemContainer = new Page(contentElement);
 
 const todoArray = new ToDoModel();
 todoArray.items = todos;
-console.log(todoArray.items);
-console.log(todoArray.addItem("Создать класс с данными"));
-console.log(todoArray.items);
-todoArray.removeItem("2");
-console.log(todoArray.items);
+
+const itemPresenter = new ItemPresenter(todoArray, Form, itemContainer, Item);
+itemPresenter.init();
+itemPresenter.renderView();
