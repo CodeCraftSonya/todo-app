925366584c88085e011adb7fb6b7616fdfe41358
DESKTOP-L88LUBN\София <sofiya.fish02@mail.ru>
2025-03-06 11:29:41 +0300

    feat: add copy and editing

--- src/components/ToDoPresenter.ts
+++ src/components/ToDoPresenter.ts
@@ -1,49 +1,84 @@
-import { IToDoModel } from "../types";
-import { IViewItemConstructor } from "./item";
-import { IForm, IFormConstructor } from "./Form";
-import { IPage } from "./Page";
+import { IToDoModel } from '../types';
+import { IViewItem, IViewItemConstructor } from './Item';
+import { IForm, IFormConstructor } from './Form';
+import { IPage } from './Page';
+import { IPopup } from './Popup';
 
 export class ItemPresenter {
-  protected itemTemplate: HTMLTemplateElement;
-  protected formTemplate: HTMLTemplateElement;
-  protected todoForm: IForm;
-  protected todoEditForm: IForm;
+	protected itemTemplate: HTMLTemplateElement;
+	protected formTemplate: HTMLTemplateElement;
+	protected todoForm: IForm;
+	protected todoEditForm: IForm;
+    protected handleSubmitEditForm: (data: {value: string}) => void;
 
-  constructor(
-    protected model: IToDoModel,
-    protected formConstructor: IFormConstructor,
-    protected viewPageConteiner: IPage,
-    protected viewItemConstructor: IViewItemConstructor
-  ) {
-    this.itemTemplate = document.querySelector(
-      "#todo-item-template"
-    ) as HTMLTemplateElement;
-    this.formTemplate = document.querySelector(
-      "#todo-form-template"
-    ) as HTMLTemplateElement;
-  }
+	constructor(
+		protected model: IToDoModel,
+		protected formConstructor: IFormConstructor,
+		protected viewPageContainer: IPage,
+		protected viewItemConstructor: IViewItemConstructor,
+		protected modal: IPopup,
+        ) {
+            this.itemTemplate = document.querySelector(
+                '#todo-item-template'
+            ) as HTMLTemplateElement;
+            this.formTemplate = document.querySelector(
+                '#todo-form-template'
+            ) as HTMLTemplateElement;
+        }
 
-  init() {
-    this.todoForm = new this.formConstructor(this.formTemplate);
-    this.todoForm.setHandler(this.handleSubmitForm.bind(this));
-    this.viewPageConteiner.formConteiner = this.todoForm.render();
-  }
+        init() {
+            this.todoForm = new this.formConstructor(this.formTemplate)
+            this.todoForm.buttonText = 'Добавить';
+            this.todoForm.placeholder = 'Следующее дело';
+            this.viewPageContainer.formContainer = this.todoForm.render();
+            this.todoEditForm = new this.formConstructor(this.formTemplate);
+            this.todoEditForm.buttonText = 'Изменить';
+            this.todoEditForm.placeholder = 'Новое название';
 
-  handleSubmitForm(data: string) {
-    this.model.addItem(data);
-    this.renderView();
-    this.todoForm.clearValue();
-  }
+            this.model.on('changed', () => {
+                this.renderView();
+            })
 
-  renderView() {
-    const itemList = this.model.items
-      .map((item) => {
-        const todoItem = new this.viewItemConstructor(this.itemTemplate);
-        const itemElement = todoItem.render(item);
-        return itemElement;
-      })
-      .reverse();
+            this.todoForm.on('submit', this.handleSubmitForm.bind(this))
+            this.todoEditForm.on('submit', (data: {value: string}) => this.handleSubmitEditForm(data))
+            }
 
-    this.viewPageConteiner.todoConteiner = itemList;
-  }
-}
+        handleSubmitForm(data: {value: string}) {
+            this.model.addItem(data.value);
+            this.todoForm.clearValue();
+        }
+    
+        handleCopyItem(item: {id: string}) {
+            const copyedItem = this.model.getItem(item.id)
+            this.model.addItem(copyedItem.name);
+        }
+    
+        handleDeleteItem(item: {id: string}) {
+            this.model.removeItem(item.id);
+        }    
+
+        handleEditItem(item: {id: string}) {
+            const editedItem = this.model.getItem(item.id)
+            this.todoEditForm.setValue(editedItem.name);
+            this.modal.content = this.todoEditForm.render();
+            this.handleSubmitEditForm = (data: {value: string}) => {
+                this.model.editItem(item.id, data.value);
+                this.todoEditForm.clearValue();
+                this.modal.close();   
+            }
+            this.modal.open();
+        }
+        
+        renderView() {
+            const itemList = this.model.items.map((item) => {
+                const todoItem = new this.viewItemConstructor(this.itemTemplate);
+    			todoItem.on('copy', this.handleCopyItem.bind(this))
+    			todoItem.on('delete', this.handleDeleteItem.bind(this))
+                todoItem.on('edit', this.handleEditItem.bind(this))
+                const itemElement = todoItem.render(item);
+                return itemElement;
+            }).reverse();
+    
+            this.viewPageContainer.todoContainer = itemList;
+        }
+    }
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
-import { Form } from "./components/Form";
-import { Item } from "./components/item";
-import { Page } from "./components/Page";
-import { ToDoModel } from "./components/ToDoModel";
-import { ItemPresenter } from "./components/ToDoPresenter";
-import "./styles/styles.css";
-import { todos } from "./utils/constants";
+import { Form } from "./components/Form"
+import { Item } from "./components/Item"
+import { Page } from "./components/Page"
+import { Popup } from "./components/Popup"
+import { ToDoModel } from "./components/ToDoModel"
+import { ItemPresenter } from "./components/ToDoPresenter"
+import "./styles/styles.css"
+import { todos } from "./utils/constants"
 
-const contentElement = document.querySelector(".content") as HTMLElement;
+const contentElement = document.querySelector('.content') as HTMLElement;
 
-const popupElement = document.querySelector(".popup") as HTMLElement;
+const popupElement = document.querySelector('.popup') as HTMLElement;
 
-const itemContainer = new Page(contentElement);
+const itemContainer = new Page(contentElement)
 
 const todoArray = new ToDoModel();
 todoArray.items = todos;
 
-const itemPresenter = new ItemPresenter(todoArray, Form, itemContainer, Item);
+const modal = new Popup(popupElement)
+
+const itemPresenter = new ItemPresenter(todoArray, Form, itemContainer, Item, modal);
+
 itemPresenter.init();
 itemPresenter.renderView();
